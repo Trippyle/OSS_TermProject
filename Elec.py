8f88effc87385768a476d32b776d80a087aeea90
chun-seyeon <144349395+chun-seyeon@users.noreply.github.com>
2023-12-14 21:37:12 +0900

    Add files via upload

--- Elec.py
+++ Elec.py
@@ -15,11 +15,15 @@ gray_image = cv2.cvtColor(combined_image, cv2.COLOR_BGR2GRAY)
 
 elec_range = cv2.inRange(combined_image, (50, 10, 170), (200, 230, 255))
 cv2.imshow('electronic_license_plate', elec_range)
-cv2.waitKey()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 commercial_range = cv2.inRange(combined_image, (20, 100, 100), (30, 255, 255))
 cv2.imshow('commercial_vehicle', commercial_range)
-cv2.waitKey()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.waitKey(0)
 
 height, width = elec_range.shape[:2]
 
@@ -53,3 +57,4 @@ if Commercial >= 1000:
     print("영업용 자동차입니다.")
 else:
     print("영업용 자동차가 아닙니다.")
+
